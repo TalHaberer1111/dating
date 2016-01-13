cbbe7fe7c86466c473c9f867ab071b6f125ad562
emmanuel cutler and tal haberer <student@epicodus.com>
2016-01-13 11:09:23 -0800

    change the html

--- js/dating.js
+++ js/dating.js
@@ -4,22 +4,22 @@ $(document).ready(function() {
     var hobbies = $("select#hobbies").val();
 
     if (age) {
-      if (age <= 21 && hobbies == 'coffee'){
+      if (age < 21 && hobbies == 'coffee'){
           $("#result1").show();
     }
   }
     if (age) {
-      if (age >= 21 && hobbies == 'coffee'){
+      if (age >=21 && hobbies == 'coffee'){
           $("#result2").show();
     }
   }
     if (age) {
-      if (age <= 21 && hobbies == 'tea'){
+      if (age < 21 && hobbies == 'tea'){
           $("#result3").show();
     }
   }
     if (age) {
-      if (age >= 21 && hobbies == 'tea'){
+      if (age > 21 && hobbies == 'tea'){
           $("#result4").show();
     }
   }
